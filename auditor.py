8bf10a75fb5d7d15022ea4ae86434adbcf8fb552
Max Lim <2604426@sit.singaporetech.edu.sg>
2026-09-11 15:18:50 +0800

    handle invalid input

--- auditor.py
+++ auditor.py
@@ -4,3 +4,11 @@ while True:
     stock_quantity = input("Enter a stock quantity: ")
     if stock_quantity == "quit":
         break
+    if not stock_quantity.isdigit():
+        print("The stock quantity entered was not a valid integer.")
+        continue
+    try:
+        stock_quantity = int(stock_quantity)
+    except ValueError:
+        print("The stock quantity entered was not a valid integer.")
+        continue

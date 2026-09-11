d5ad6299b82750318a0e7c3bbaa1b71855aa1de5
Max Lim <2604426@sit.singaporetech.edu.sg>
2026-09-11 15:24:37 +0800

    reporting, finalise program

--- auditor.py
+++ auditor.py
@@ -1,16 +1,19 @@
 inventory = 0
+failed = 0
 
 while True:
-    stock_quantity = input("Enter a stock quantity: ")
+    stock_quantity = input("Enter stock quantity: ")
     if stock_quantity == "quit":
         break
     if not stock_quantity.isdigit():
         print("The stock quantity entered was not a valid integer.")
+        failed += 1
         continue
     try:
         stock_quantity = int(stock_quantity)
     except ValueError:
         print("The stock quantity entered was not a valid integer.")
+        failed += 1
         continue
     if stock_quantity < 0:
         print("The stock quantity cannot be a negative number.")
@@ -21,3 +24,5 @@ while True:
     if inventory > 500:
         print("Overstock alert: the inventory has exceeded 500 units (" + str(inventory) + ")")
         break
+print("Total Units Processed:", inventory)
+print("Number of Failed/Rejected Entries:", failed)
